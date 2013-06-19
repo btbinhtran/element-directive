7f5c1e346b636c2342431fd6055e1a646f2d363f
Lance Pollard <lancejpollard@gmail.com>
2013-06-19 15:35:49 -0700

    basics

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ var directive = require('tower-directive');
  * Expose `elementDirective`.
  */
 
-module.exports = directive('data-element', elementDirective);
+module.exports = directive('data-element').compiler(elementDirective);
 
 /**
  * Define `elementDirective`.
  */
 
-function elementDirective(scope, el, attr) {
+function elementDirective(el, attr) {
+  var expression = attr.expression;
+  // XXX: could probably use some way of caching.
 
+  function exec(scope, cursor, attr) {
+    var name = expression(scope);
+    var obj = element(name).init({ parent: scope });
+    var el = obj.render();
+    cursor.parentNode.insertBefore(el, cursor.nextSibling);
+  }
+
+  return exec;
 }
